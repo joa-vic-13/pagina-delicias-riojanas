370fa2a27c7b2c3df086897b40e9c21cf515627e
Joaquin Vicente <jnvicente@gmail.com>
2025-10-23 11:01:08 -0300

    Primera versión

---
+++ products.py
@@ -0,0 +1,16 @@
+products = [
+    {
+        "id": 1,
+        "name": "Producto A",
+        "price": "AR$ 1.200",
+        "description": "Descripción corta del producto A.",
+        "image": "images/prueba.jpg"
+    },
+    {
+        "id": 2,
+        "name": "Producto B",
+        "price": "AR$ 950",
+        "description": "Descripción corta del producto B.",
+        "image": "images/vertical.jpg"
+    }
+]
