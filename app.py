48886f63368c9b5b5d37cc3a212da96d3407bba8
Joaquin Vicente <jnvicente@gmail.com>
2025-10-27 07:47:23 -0300

    adds requirements.txt

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 from products import products
+import os
 
 app = Flask(__name__)
 
@@ -8,4 +9,4 @@ def index():
     return render_template("index.html", products=products)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
